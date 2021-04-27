044d35aaf40d4be2bd5f864d5281abf74e0ef51b
Tofayel Hyder <abhihyder7@gmail.com>
2021-04-28 02:29:47 +0600

    theme layout done

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <Sidebar></Sidebar>
+      <div class="main-panel">
+        <Header></Header>
+        <Dashboard></Dashboard>
+        <Footer></Footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/layouts/Header.vue";
+import Footer from "./components/layouts/Footer.vue";
+import Sidebar from "./components/layouts/Sidebar.vue";
+import Dashboard from "./components/pages/Dashboard.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+    Sidebar,
+    Dashboard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
